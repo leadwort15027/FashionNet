1d51a412859da4896c141de5feda473737120cf8
leadwort15027 <--global>
2017-02-24 11:04:50 +0800

    finish branch pose

--- fashionnet.py
+++ fashionnet.py
@@ -1,36 +1,60 @@
 from keras.models import Model
-from keras.layers import Input, Dense, Activation, Flatten, Convolution2D, MaxPooling2D
+from keras.layers import Input, Dense, Activation, Flatten, Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.utils.visualize_util import plot
 import matplotlib.pyplot as plt
 
-input_shape = (228,228,3)
+input_shape = (224,224,3)
 inputs = Input(shape=input_shape, name='inputs')
 x = Convolution2D(64, 3, 3, activation='relu', name='conv1_1')(inputs)
+x = ZeroPadding2D(padding=(1,1), name='pad1_1')(x)
 x = Convolution2D(64, 3, 3, activation='relu', name='conv1_2')(x)
+x = ZeroPadding2D(padding=(1,1), name='pad1_2')(x)
 x = MaxPooling2D(pool_size=(2,2), name='pool1')(x)
 
 x = Convolution2D(128, 3, 3, activation='relu', name='conv2_1')(x)
+x = ZeroPadding2D(padding=(1,1), name='pad2_1')(x)
 x = Convolution2D(128, 3, 3, activation='relu', name='conv2_2')(x)
+x = ZeroPadding2D(padding=(1,1), name='pad2_2')(x)
 x = MaxPooling2D(pool_size=(2,2), name='pool2')(x)
 
 x = Convolution2D(256, 3, 3, activation='relu', name='conv3_1')(x)
+x = ZeroPadding2D(padding=(1,1), name='pad3_1')(x)
 x = Convolution2D(256, 3, 3, activation='relu', name='conv3_2')(x)
+x = ZeroPadding2D(padding=(1,1), name='pad3_2')(x)
 x = Convolution2D(256, 3, 3, activation='relu', name='conv3_3')(x)
+x = ZeroPadding2D(padding=(1,1), name='pad3_3')(x)
 x = MaxPooling2D(pool_size=(2,2), name='pool3')(x)
 
 x = Convolution2D(512, 3, 3, activation='relu', name='conv4_1')(x)
+x = ZeroPadding2D(padding=(1,1), name='pad4_1')(x)
 x = Convolution2D(512, 3, 3, activation='relu', name='conv4_2')(x)
-conv4 = Convolution2D(512, 3, 3, activation='relu', name='conv4_3')(x)
-x = MaxPooling2D(pool_size=(2,2), name='pool4')(conv4)
+x = ZeroPadding2D(padding=(1,1), name='pad4_2')(x)
+x = Convolution2D(512, 3, 3, activation='relu', name='conv4_3')(x)
+pad4_3 = ZeroPadding2D(padding=(1,1), name='pad4_3')(x)
+# pad4_3 -> conv5_local
+x = MaxPooling2D(pool_size=(2,2), name='pool4')(pad4_3)
 
-x = Convolution2D(512, 3, 3, activation='relu', name='conv5_1')(x)
-x = Convolution2D(512, 3, 3, activation='relu', name='conv5_2')(x)
-x = Convolution2D(512, 3, 3, activation='relu', name='conv5_3')(x)
-x = MaxPooling2D(pool_size=(2,2), name='pool5')(x)
+# POSE
+p = Convolution2D(512, 3, 3, activation='relu', name='conv5_pose_1')(x)
+p = ZeroPadding2D(padding=(1,1), name='pad5_pose_1')(p)
+p = Convolution2D(512, 3, 3, activation='relu', name='conv5_pose_2')(p)
+p = ZeroPadding2D(padding=(1,1), name='pad5_pose_2')(p)
+p = Convolution2D(512, 3, 3, activation='relu', name='conv5_pose_3')(p)
+p = ZeroPadding2D(padding=(1,1), name='pad5_pose_3')(p)
+p = MaxPooling2D(pool_size=(2,2), name='pool5')(p)
+p = Flatten()(p)
+p = Dense(1024, activation='relu', name='fc6_pose')(p)
+p = Dense(1024, activation='relu', name='fc7_pose')(p)
+loc = Dense(8, activation='linear',name='location')(p)
+vis1 = Dense(2, activation='softmax', name='vis1')(p)
+vis2 = Dense(2, activation='softmax', name='vis2')(p)
+vis3 = Dense(2, activation='softmax', name='vis3')(p)
+vis4 = Dense(2, activation='softmax', name='vis4')(p)
 
-x = Dense(1024, activation='relu', name='fc6_pose')(x)
-outputs = Dense(1024, activation='relu', name='fc7_pose')(x)
 
+
+
+outputs=[loc,vis1,vis2,vis3,vis4]
 
 model = Model(input=inputs, output=outputs)
 model.summary()
